07a714c164dec4724e3a17f42c345c798bb5898e
EcoRappTech <ecorapptech@gmail.com>
2026-01-09 12:09:37 +0100

    Update contact.js

--- functions/contact.js
+++ functions/contact.js
@@ -1,29 +1,14 @@
 export async function onRequestPost({ request }) {
-  try {
-    const data = await request.formData();
+  const data = await request.formData();
 
-    const tipo = data.get("tipo");
-    const nome = data.get("nome");
-    const email = data.get("email");
-    const messaggio = data.get("messaggio");
-
-    return new Response(
-      JSON.stringify({
-        status: "ok",
-        tipo,
-        nome,
-        email,
-        messaggio
-      }),
-      {
-        status: 200,
-        headers: { "Content-Type": "application/json" }
-      }
-    );
-  } catch (err) {
-    return new Response(
-      "ERRORE: " + err.message,
-      { status: 500 }
-    );
-  }
+  return new Response(
+    JSON.stringify({
+      status: "ok",
+      ricevuto: true
+    }),
+    {
+      status: 200,
+      headers: { "Content-Type": "application/json" }
+    }
+  );
 }
